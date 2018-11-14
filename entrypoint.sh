48b0a50c7af304f9859130b7c4e1be97a8636bf3
Hiroaki Nakamura <hnakamur@gmail.com>
2018-11-14 10:21:09 +0900

    Add environment variable USE_GOSU to choose not to use go

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-USER_ID=${LOCAL_UID:-9001}
-GROUP_ID=${LOCAL_GID:-9001}
-
-echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
-# NOTE: We use system user here to skip setting password prompt.
-# And we use system group just for consistency with user.
-addgroup -S -g $GROUP_ID user
-adduser -S -u $USER_ID -G user user
-export HOME=/home/user
-
-exec /usr/bin/gosu user "$@"
+if [ "${USE_GOSU:-0}" -eq 0 ]; then
+  cmd=""
+else
+  USER_ID=${LOCAL_UID:-9001}
+  GROUP_ID=${LOCAL_GID:-9001}
+  
+  echo "Starting with UID : $USER_ID, GID: $GROUP_ID"
+  # NOTE: We use system user here to skip setting password prompt.
+  # And we use system group just for consistency with user.
+  addgroup -S -g $GROUP_ID user
+  adduser -S -u $USER_ID -G user user
+  export HOME=/home/user
+  
+  cmd="/usr/bin/gosu user"
+fi
+exec $cmd "$@"
