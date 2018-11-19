618ba0d6927205539ddc288ca64d197e3c0a5896
Hiroaki Nakamura <hnakamur@gmail.com>
2018-11-19 10:18:25 +0900

    Fix string comparison in shell scripts

--- bin/sphinx-run.sh
+++ bin/sphinx-run.sh
@@ -3,7 +3,7 @@ dockertag="${DOCKER_TAG:-hnakamur/sphinx}"
 if [ -n "$BASE_DIR" ]; then
   cd "$BASE_DIR"
 fi
-if [ "$(uname -s)" == "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
   uid_opts="-e USE_GOSU=0"
 else
   uid_opts="-e USE_GOSU=1 -e LOCAL_UID=$(id -u $USER) -e LOCAL_GID=$(id -g $USER)"
